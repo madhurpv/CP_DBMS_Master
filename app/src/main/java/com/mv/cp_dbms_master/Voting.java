b6af5849615aa2623c736f8f3624e694a8a85868
madhurpv <madhurpreetamvaidya@gmail.com>
2024-04-28 17:38:27 +0530

    Working Votings, Small Changes

--- app/src/main/java/com/mv/cp_dbms_master/Voting.java
+++ app/src/main/java/com/mv/cp_dbms_master/Voting.java
@@ -2,25 +2,36 @@ package com.mv.cp_dbms_master;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.AlertDialog;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -33,6 +44,7 @@ public class Voting extends AppCompatActivity implements VotingRecyclerAdapter.I
 
     public static VotingRecyclerAdapter adapter;
     RecyclerView votingsRecyclerView;
+    Button addNewButton;
 
 
     // creating a variable for our
@@ -97,7 +109,12 @@ public class Voting extends AppCompatActivity implements VotingRecyclerAdapter.I
                         Map<String, Long> options_map = (Map<String, Long>) noticeData.get("Options");
                         List<String> options = new ArrayList<>(options_map.keySet());
                         HashMap<String, Long> votedFlats = (HashMap<String, Long>) noticeData.get("Voters");
-                        Log.d("QWER", votedFlats.toString());
+                        if(votedFlats == null){
+                            votedFlats = new HashMap<>();
+                        }
+                        else{
+                            Log.d("QWER", votedFlats.toString());
+                        }
 
                         boolean found = false;
                         int flatNo = sharedPreferences.getInt("flatNo", -1);
@@ -123,6 +140,15 @@ public class Voting extends AppCompatActivity implements VotingRecyclerAdapter.I
             }
         });
 
+        addNewButton = findViewById(R.id.addNewButton);
+        addNewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                showCustomDialog(Voting.this);
+
+            }
+        });
 
     }
 
@@ -187,6 +213,127 @@ public class Voting extends AppCompatActivity implements VotingRecyclerAdapter.I
 
 
 
+    }
+
+    // Method to show AlertDialog
+    public void showCustomDialog(Context context) {
+
+        final Calendar[] startDate = {null};
+        final Calendar[] endDate = {null};
+
+        // Inflate the custom layout
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View dialogView = inflater.inflate(R.layout.votings_alertdialog, null);
+
+        // Initialize the views from the custom layout
+        CardView cardView = dialogView.findViewById(R.id.cardView);
+        TextView textView = dialogView.findViewById(R.id.textView);
+        EditText editTextName = dialogView.findViewById(R.id.editTextName);
+        EditText editTextDetails = dialogView.findViewById(R.id.editTextDetails);
+        EditText editTextOption1 = dialogView.findViewById(R.id.editTextOption1);
+        EditText editTextOption2 = dialogView.findViewById(R.id.editTextOption2);
+        Button pickStartTimeButton = dialogView.findViewById(R.id.pickStartTimeButton);
+        Button pickEndTimeButton = dialogView.findViewById(R.id.pickEndTimeButton);
+        Button submitButton = dialogView.findViewById(R.id.submitButton);
+        submitButton.setEnabled(false);
+
+        // Create the AlertDialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setView(dialogView);
+
+        pickStartTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar currentDate = Calendar.getInstance();
+                startDate[0] = Calendar.getInstance();
+                new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        startDate[0].set(year, monthOfYear, dayOfMonth);
+                        new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                startDate[0].set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                startDate[0].set(Calendar.MINUTE, minute);
+                                Log.v("QWER", "The choosen one " + startDate[0].getTime());
+                                pickStartTimeButton.setText("Chosen Date : " + startDate[0].getTime());
+                                if(endDate[0] != null && startDate[0].compareTo(endDate[0]) < 0 && !editTextName.getText().toString().equals("") && !editTextDetails.getText().toString().equals("") && !editTextOption1.getText().toString().equals("") && !editTextOption2.getText().toString().equals("")){
+                                    submitButton.setEnabled(true);
+                                }
+                            }
+                        }, currentDate.get(Calendar.HOUR_OF_DAY), currentDate.get(Calendar.MINUTE), false).show();
+                    }
+                }, currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH), currentDate.get(Calendar.DATE)).show();
+            }
+        });
+
+
+        pickEndTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Calendar currentDate = Calendar.getInstance();
+                endDate[0] = Calendar.getInstance();
+                new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        endDate[0].set(year, monthOfYear, dayOfMonth);
+                        new TimePickerDialog(context, new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                endDate[0].set(Calendar.HOUR_OF_DAY, hourOfDay);
+                                endDate[0].set(Calendar.MINUTE, minute);
+                                Log.v("QWER", "The choosen one " + endDate[0].getTime());
+                                pickEndTimeButton.setText("Chosen Date : " + endDate[0].getTime());
+                                if(startDate[0] != null && startDate[0].compareTo(endDate[0]) < 0 && !editTextName.getText().toString().equals("") && !editTextDetails.getText().toString().equals("") && !editTextOption1.getText().toString().equals("") && !editTextOption2.getText().toString().equals("")){
+                                    submitButton.setEnabled(true);
+                                }
+                            }
+                        }, currentDate.get(Calendar.HOUR_OF_DAY), currentDate.get(Calendar.MINUTE), false).show();
+                    }
+                }, currentDate.get(Calendar.YEAR), currentDate.get(Calendar.MONTH), currentDate.get(Calendar.DATE)).show();
+            }
+        });
+
+        AlertDialog dialog = builder.create();
+
+        // Set up the submit button click listener
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle the submission here
+                HashMap<String, Object> pollDetails = new HashMap<>();
+                pollDetails.put("Details", editTextDetails.getText().toString());
+
+                HashMap<String, Object> optionsDetails = new HashMap<>();
+                optionsDetails.put(editTextOption1.getText().toString(), "");
+                optionsDetails.put(editTextOption2.getText().toString(), "");
+                pollDetails.put("Options", optionsDetails);
+
+                pollDetails.put("startTime", startDate[0].getTimeInMillis());
+                pollDetails.put("endTime", endDate[0].getTimeInMillis());
+
+
+                databaseReference.child("Votings").child(editTextName.getText().toString()).updateChildren(pollDetails).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Toast.makeText(context, "Details Updated!", Toast.LENGTH_SHORT).show();
+                        dialog.dismiss();
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+
+                        //Toast.makeText(SignUpActivity.this, "Some Error Occured", Toast.LENGTH_SHORT).show();
+                        Log.d("QWER", "Error : " + e);
+
+                    }
+                });
+            }
+        });
+
+        // Create and show the AlertDialog
+        dialog.show();
     }
 
 
